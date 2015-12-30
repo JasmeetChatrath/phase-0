84cf7f51b7c8f31fdf8caf66076839f9c9907d01
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2015-12-30 12:39:13 -0800

    Add 4.3 reflection

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -12,3 +12,27 @@ puts "What is your favorite number?"
 number = gets.chomp.to_i + 1
 
 puts "Thats ok but " + number.to_s + " is better."
+
+=begin
+How do you define a local variable?
+To define a local variable we assign the name of our variable to it's value using an "=".
+
+How do you define a method?
+To define a method we enter def, the the method name, then the parameters,  what we want it to do, and then close it with end.
+
+What is the difference between a local variable and a method?
+A method is a block of code used by an object while a variable stores values.
+
+How do you run a ruby program from the command line?
+We enter ruby and the file name.
+
+How do you run an RSpec file from the command line?
+We run rspec and then the file name.
+
+What was confusing about this material? What made sense?
+This section was straight forward because it was mostly review.
+
+=end
+https://github.com/JasmeetChatrath/phase-0/tree/master/week-4/address
+https://github.com/JasmeetChatrath/phase-0/tree/master/week-4/math
+https://github.com/JasmeetChatrath/phase-0/tree/master/week-4/define-method
