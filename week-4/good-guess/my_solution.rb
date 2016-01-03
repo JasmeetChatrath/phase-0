582ec3ed967aae630e403694c7a22aa5c38fd94d
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2016-01-03 15:04:30 -0800

    Add 4.5.1 and 4.5.4

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,14 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+by myself
+
+# Your Solution Below
+
+def good_guess? (integer)
+  if integer == 42
+    puts true
+  else
+    puts false
+  end
+end
